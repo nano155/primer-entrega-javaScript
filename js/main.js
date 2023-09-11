8750083e4f87e898e824b297149805d79e0c024a
Urbano Molina <arq.molina@live.com>
2023-09-10 21:00:17 -0500

    correccion 2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
+alert(`**********Bienvenidos a gift makers*************`);
 const mostrar = () => {
-  alert(`**********Bienvenidos a gift makers************* \n 
-  selecciona la opcion que quieras usar \n
+  dato = parseInt(prompt(`selecciona la opcion que quieras usar \n
          1. Para ver la opcion de tortas \n
          2. Para ver la opcion de postres \n
          3. Para ver la opcion de caketopper \n
-         4. para salir y ver el total de tu pedido`);
-  dato = parseInt(prompt("Digite su opcion"));
+         4. para salir y ver el total de tu pedido`));
+
   return dato;
 };
 let opcion = mostrar();
@@ -29,11 +29,10 @@ while (opcion !== 4) {
   if (validacion == true) {
     switch (opcion) {
       case 1:
-        alert(`Elegiste la opcion tortas, digite el pastel que quiere agregar a su carro de compras \n
+       dato = parseInt(prompt(`Elegiste la opcion tortas, digite el pastel que quiere agregar a su carro de compras \n
                                         1. torta de chocolate $20.000 \n
                                         2. torta de vainilla $25.000 \n
-                                        3. torta de durazno $30.000`);
-        dato = parseInt(prompt());
+                                        3. torta de durazno $30.000`));
         comprobante(dato);
         if (validacion == true) {
           switch (dato) {
@@ -56,10 +55,9 @@ while (opcion !== 4) {
         }
         break;
       case 2:
-        alert(`Elegiste la opcion postres, digite el postre que quiere agregar a su carro de compras \n
+        dato = parseInt(prompt(`Elegiste la opcion postres, digite el postre que quiere agregar a su carro de compras \n
           1. postre de 3 leches $15.000 \n
-          2. postre de gelatina $18.000`);
-        dato = parseInt(prompt());
+          2. postre de gelatina $18.000`));
         comprobante(dato);
         if (validacion == true) {
           switch (dato) {
@@ -77,9 +75,8 @@ while (opcion !== 4) {
         }
         break;
       case 3:
-        alert(`Elegiste la opcion arreglos, digite a continuacion si desea nuestros servicios \n
-        1. nuestro servicio de arreglos tiene un valor de $23.000`);
-        dato = parseInt(prompt());
+        dato = parseInt(prompt(`Elegiste la opcion arreglos, digite a continuacion si desea nuestros servicios \n
+        1. nuestro servicio de arreglos tiene un valor de $23.000`));
         console.log(dato);
         comprobante(dato);
         if (validacion == true) {
@@ -104,19 +101,22 @@ while (opcion !== 4) {
   comprobante(opcion);
 }
 
-
-const total = carritoCompras.reduce((a , b)=>{
-
-  return a+b;
-});
-let items=[];
-
-for (let index = 0; index < carritoCompras.length; index++) {
-  const element = carritoCompras[index].toString();
-  const element2 = carritoItems[index].toString();
+if (carritoCompras.length > 0){
+  const total = carritoCompras.reduce((a , b)=>{
   
-
-items =items + (` Item ${element2} : ${element}\n`)
+    return a+b;
+  });
+  
+  let items=[];
+  
+  for (let index = 0; index < carritoCompras.length; index++) {
+    const element = carritoCompras[index].toString();
+    const element2 = carritoItems[index].toString();
+    
+  
+  items =items + (` Item ${element2} : ${element}\n`)
+  }
+  alert(`Usted compro : \n${items}\nEl total de su compra fue de $${total}`);
+}else{
+  alert('El total de su compra es 0');
 }
-
-alert(`Usted compro : \n${items}\nEl total de su compra fue de $${total}`)
